c46d30f8fa70324ae74bb1d2e63cea7bfcfc2166
gabPetti <gabpettidev@gmail.com>
2023-03-11 07:18:27 -0300

    Create navbar component and style it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Casual from "./pages/casual/Casual"
+import "./App.css"
 
 function App() {
   return (
--- client/src/pages/casual/Casual.jsx
+++ client/src/pages/casual/Casual.jsx
@@ -1,11 +1,11 @@
+import Navbar from "../../components/navbar/Navbar"
 import "./casual.sass";
 
 export default function Casual() {
   return (
     <>
       <Navbar />
-      <Typetest />
-      
+      {/* <Typetest /> */}
     </>
   );
 }
