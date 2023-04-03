7ec7c4b6bd1d5ffbeac2614941aac906cd0a5034
gabPetti <gabpettidev@gmail.com>
2023-04-02 22:38:06 -0300

    Add new features
    Space feature, invisible input and extra words error marking

--- client/src/components/typetest/Typetest.jsx
+++ client/src/components/typetest/Typetest.jsx
@@ -1,91 +1,92 @@
 import { useState, useEffect } from "react";
-import wordList from './words.json';
+import wordList from "./words.json";
 import "./typetest.sass";
 
 export default function Typetest() {
   var generatedWords;
   const [words, setWords] = useState([]);
-  const [carret, setCarret] = useState(0)
-  const [activeWordIndex, setActiveWordIndex] = useState(0)
-  const [activeLetterIndex, setActiveLetterIndex] = useState(0)
-  const [activeLetter, setActiveLetter] = useState("")
-  const [currentInput, setCurrentInput] = useState("")
-  const [currentLetter, setCurrentLetter] = useState(0)
-  
+  const [carret, setCarret] = useState(0);
+  const [activeWordIndex, setActiveWordIndex] = useState(0);
+  const [activeLetterIndex, setActiveLetterIndex] = useState(0);
+  const [activeLetter, setActiveLetter] = useState("");
+  const [currentInput, setCurrentInput] = useState("");
+  const [currentLetter, setCurrentLetter] = useState(0);
+
   useEffect(() => {
     generatedWords = [];
     for (let i = 0; i < 200; i++) {
-      generatedWords.push(wordList.words[Math.floor(Math.random() * 9904)].split(""))
+      generatedWords.push(
+        wordList.words[Math.floor(Math.random() * 9904)].split("")
+      );
       for (let j = 0; j < generatedWords[i].length; j++) {
         generatedWords[i][j] = {
           text: generatedWords[i][j],
-          state: ""
-        }
+          state: "",
+        };
       }
     }
-    setWords(generatedWords)
-  }, [])
-  
+    setWords(generatedWords);
+  }, []);
+
   function handleKeyDown(event) {
-    // space bar 
-    var key = event.key
-    var keyCode = event.code
+    // space bar
     // checkValid(event,key)
-    if (keyCode === 32) {
-      setCurrInput("")
-      setActiveWordIndex(activeWordIndex + 1)
-      setActiveLetterIndex(0)
-    // backspace
-    } else if (event.key === "Backspace") {
-      setActiveLetterIndex(activeLetterIndex - 1)
-      setActiveLetter(key)
-      let newWords = [...words]
-      newWords[activeWordIndex][activeLetterIndex - 1].state = "";
-      setWords(newWords)
-      console.log("backspacezin")
+    if (event.key == " ") {
+      setActiveWordIndex(activeWordIndex + 1);
+      setActiveLetterIndex(0);
+      // backspace
+    } else if (event.key == "Backspace") {
+      if (activeLetterIndex == 0) {
+        setActiveWordIndex(activeWordIndex - 1);
+        setActiveLetterIndex(0);
+      } else if (words[activeWordIndex][activeLetterIndex - 1].state == "extra") {
+        setActiveLetterIndex(activeLetterIndex - 1);
+        let newWords = [...words];
+        newWords[activeWordIndex].pop();
+        setWords(newWords);
+      } else {
+        setActiveLetterIndex(activeLetterIndex - 1);
+        setActiveLetter(event.key);
+        let newWords = [...words];
+        newWords[activeWordIndex][activeLetterIndex - 1].state = "";
+        setWords(newWords);
+      }
     } else {
-      setActiveLetterIndex(activeLetterIndex + 1)
-      setActiveLetter(key)
-      wordChecker(key);
+      setActiveLetterIndex(activeLetterIndex + 1);
+      setActiveLetter(event.key);
+      wordChecker(event.key);
     }
   }
 
   function wordChecker(key) {
-    const current = words[activeWordIndex][activeLetterIndex].text
-    console.log(`you had to type ${current} and you typed ${key}`)
-    if (current == undefined) {
-      words[activeWordIndex].push(key)
-    }
-    const match = (current == key)
-    if (current == key) {
-      let newWords = [...words]
-      newWords[activeWordIndex][activeLetterIndex].state = "correct"
-      setWords(newWords)
+    if (activeLetterIndex >= words[activeWordIndex].length) {
+      let newWords = [...words];
+      newWords[activeWordIndex].push({ text: key, state: "extra" });
+      setWords(newWords);
     } else {
-      let newWords = [...words]
-      newWords[activeWordIndex][activeLetterIndex].state = "incorrect"
-      setWords(newWords)
+      const current = words[activeWordIndex][activeLetterIndex].text;
+      if (current == key) {
+        let newWords = [...words];
+        newWords[activeWordIndex][activeLetterIndex].state = "correct";
+        setWords(newWords);
+      } else {
+        let newWords = [...words];
+        newWords[activeWordIndex][activeLetterIndex].state = "incorrect";
+        setWords(newWords);
+      }
     }
-    console.log({match})
   }
 
-  function checkValid(event,key) {
+  function checkValid(event, key) {
     var regex = new RegExp("^[a-zA-Z0-9]+$");
-    var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+    var key = String.fromCharCode(
+      !event.charCode ? event.which : event.charCode
+    );
     if (!regex.test(key)) {
-      event.preventDefault(); return false;
+      event.preventDefault();
+      return false;
     }
   }
-
-
-  // function handleKeyDown(event) {
-  //   var name = event.key;
-  //   var code = event.code;
-  //   wordChecker(name);
-  //   setCurrentLetter(currentLetter + 1)
-  //   // Alert the key name and key code on keydown
-  //   // alert(`Key pressed ${name} \r\n Key code value: ${code}`);
-  // };
 
   return (
     <div className="typetestContainer">
@@ -97,19 +98,27 @@ export default function Typetest() {
           <span id="time">63</span>
           <span>&nbsp;s</span>
         </div>
-        <div className="typetestText" id='wordsList'>
-          <input type="text" pattern="[A-Za-z]" onKeyDown={handleKeyDown} value={currentInput} onChange={(e) => setCurrentInput(e.target.value)}/>
-          {/* <input type="text" value={currentInput} onChange={(e) => setCurrentInput(e.target.value)}/> */}
-          {words.map( (word, i) => 
+        <div className="typetestText" id="wordsList">
+          <input
+            type="text"
+            pattern="[A-Za-z]"
+            autoFocus={true}
+            onBlur={({ target }) => target.focus()}
+            onKeyDown={handleKeyDown}
+            value={currentInput}
+            onChange={(e) => setCurrentInput(e.target.value)}
+          />
+          {words.map((word, i) => (
             <div key={i} className="word">
-              {word.map( (letter, j) => (
-                <span className={letter.state} key={j}>{letter.text}</span>
+              {word.map((letter, j) => (
+                <span className={letter.state} key={j}>
+                  {letter.text}
+                </span>
               ))}
             </div>
-          )}
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
-
