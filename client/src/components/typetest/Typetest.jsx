f68a1c79518253671c57a90a0f623acfe2988578
gabPetti <gabpettidev@gmail.com>
2023-05-15 13:33:01 -0300

    Remake entire typetest and to separate words

--- client/src/components/typetest/Typetest.jsx
+++ client/src/components/typetest/Typetest.jsx
@@ -4,51 +4,74 @@ import "./typetest.sass";
 
 export default function Typetest() {
   const [words, setWords] = useState([]);
-  var generatedWords;
-  var wordGenerated;
+  const [initialWords, setInitialWords] = useState([]);
 
   // Generate words to the test
   useEffect(() => {
-    generatedWords = [];
+    var generatedWords = [];
+    var wordGenerated;
     for (let i = 0; i < 200; i++) {
       wordGenerated = wordList.words[Math.floor(Math.random() * 9904)].split("");
       for (let j = 0; j < wordGenerated.length; j++) {
         wordGenerated[j] = {
           text: wordGenerated[j],
-          status: " "
+          status: "",
         };
       }
-      generatedWords = generatedWords.concat(wordGenerated)
-      generatedWords.push({text: "\xa0", status: ""})
+      generatedWords = generatedWords.concat([wordGenerated]);
     }
-    generatedWords[0].status = "active"
-    setWords([...words, ...generatedWords])
-  }, [generatedWords]);
+    generatedWords[0][0].status = "active";
+    setInitialWords([...initialWords, ...generatedWords]);
+    setWords([...words, ...generatedWords]);
+  }, []);
 
+  var spaces = 0;
   // Compare input text with the words of the test
-  const handleInput = event => {
+  const handleInput = (event) => {
     var userInput = event.target.value;
-    var maxInput = userInput.length;
-    var copyWords = words;
-    for (let i = 0; i < maxInput; i++) {
-      if (userInput[i] == words[i].text) {
-        copyWords[i].status = "correct"
-      } else if (userInput[i] == " " && words[i].text == "\xa0") {
-        copyWords[i].status = "correct"
-      } else if (userInput[i] != words[i].text) {
-        copyWords[i].status = "incorrect"
+    userInput = userInput.split(" ");
+    var phraseLength = userInput.length;
+    userInput = userInput.map((x) => x.split(""));
+    var wordLength = userInput[phraseLength - 1].length;
+    var copyWords = initialWords;
+    console.log(userInput)
+    
+    // word input checker
+    for (let i = 0; i < userInput.length; i++) {
+      for (let j = 0; j < Math.max(userInput[i].length, initialWords[i].length); j++) {
+        if (initialWords[i][j] != undefined && userInput[i][j] == undefined) {
+          copyWords[i][j].status = "";
+        } else if (initialWords[i][j] == undefined && userInput[i][j] != undefined) {
+          copyWords[i][j] = { text: userInput[i][j].text, status: "extra" };
+        } else if (userInput[i][j] != initialWords[i][j].text) {
+          copyWords[i][j].status = "incorrect";
+        } else if (userInput[i][j] == initialWords[i][j].text) {
+          copyWords[i][j].status = "correct";
+        }
       }
-      setWords([...copyWords])
     }
 
-    while(copyWords[maxInput].status != " ") {
-      copyWords[maxInput].status = " "
-      maxInput++
+    // backspace inside word checker
+    if (words[phraseLength - 1][wordLength] != undefined) {
+      if (words[phraseLength - 1][wordLength].status != "") {
+        copyWords[phraseLength - 1][wordLength].status = "";
+      }
     }
 
-    copyWords[userInput.length].status = "active"
-    setWords([...copyWords])
-  }
+    // backspace between words checker
+    if (words[phraseLength][0].status != "") {
+      copyWords[phraseLength] = copyWords[phraseLength].map(x => (x.status = ""))
+    }
+
+    // set current active word
+    // if (copyWords[phraseLength - 1][wordLength] == undefined) {
+    //   copyWords[phraseLength][0].status = "active"
+    // } else {
+    //   copyWords[phraseLength - 1][wordLength].status = "active";
+    // }
+
+    setWords([...copyWords]);
+  };
 
   return (
     <div className="typetestContainer">
@@ -70,10 +93,15 @@ export default function Typetest() {
           />
         </div>
         <div className="typetestText" id="wordsList">
-          <div id="carret"></div>
-            {words.map((letter, i) => (
-              <span style={letter.status == "active" ? {position: "relative"} : {}} className={letter.status + " "} key={i}>{letter.text}</span>
-            ))}
+          {words.map((word, i) => (
+            <div className="words" key={i}>
+              {word.map((letter, j) => (
+                <span className={letter.status} key={j}>
+                  {letter.text}
+                </span>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
