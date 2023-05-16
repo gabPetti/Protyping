2711d1035829b13865f7c68fd25b01dda7badb2b
gabPetti <gabpettidev@gmail.com>
2023-05-16 12:50:22 -0300

    Fix extra letter blending with initial words

--- client/src/components/typetest/Typetest.jsx
+++ client/src/components/typetest/Typetest.jsx
@@ -4,13 +4,14 @@ import "./typetest.sass";
 
 export default function Typetest() {
   const [words, setWords] = useState([]);
-  const [initialWords, setInitialWords] = useState([]);
+  const [time, setTime] = useState(30);
+  const [started, setStarted] = useState(false);
 
   // Generate words to the test
   useEffect(() => {
     var generatedWords = [];
     var wordGenerated;
-    for (let i = 0; i < 200; i++) {
+    for (let i = 0; i < 50; i++) {
       wordGenerated = wordList.words[Math.floor(Math.random() * 9904)].split("");
       for (let j = 0; j < wordGenerated.length; j++) {
         wordGenerated[j] = {
@@ -21,70 +22,52 @@ export default function Typetest() {
       generatedWords = generatedWords.concat([wordGenerated]);
     }
     generatedWords[0][0].status = "active";
-    setInitialWords([...initialWords, ...generatedWords]);
-    setWords([...words, ...generatedWords]);
+    setWords([...generatedWords])
   }, []);
-
-  var spaces = 0;
   // Compare input text with the words of the test
   const handleInput = (event) => {
     var userInput = event.target.value;
     userInput = userInput.split(" ");
-    var phraseLength = userInput.length;
     userInput = userInput.map((x) => x.split(""));
-    var wordLength = userInput[phraseLength - 1].length;
-    var copyWords = initialWords;
-    console.log(userInput)
-    
+    var copyWords = words;
     // word input checker
     for (let i = 0; i < userInput.length; i++) {
-      for (let j = 0; j < Math.max(userInput[i].length, initialWords[i].length); j++) {
-        if (initialWords[i][j] != undefined && userInput[i][j] == undefined) {
-          if (copyWords[i][j].status == "extra") {
-            copyWords[i].pop()// = {text: "", status: ""};
-          } else {
-            copyWords[i][j].status = "";
-          }
-        } else if (initialWords[i][j] == undefined && userInput[i][j] != undefined) {
-          copyWords[i].push({ text: userInput[i][j], status: "extra" });
-        } else if (userInput[i][j] != initialWords[i][j].text && copyWords[i][j].status != "extra") {
+      for (let j = 0; j < Math.max(userInput[i].length, words[i].length); j++) {
+        if (userInput[i][j] == undefined && words[i][j].status == "extra") {
+          copyWords[i].pop();
+        } else if (j > userInput[i].length - 1) {
+          copyWords[i][j].status = "";
+        } else if (words[i][j] == undefined) {
+          copyWords[i][j] = {text: userInput[i][j], status: "extra"};
+        // right key pressed
+        } else if (userInput[i][j] != words[i][j].text && copyWords[i][j].status != "extra") {
           copyWords[i][j].status = "incorrect";
-        } else if (userInput[i][j] == initialWords[i][j].text && copyWords[i][j].status != "extra") {
+        // right key pressed
+        } else if (userInput[i][j] == words[i][j].text && copyWords[i][j].status != "extra") {
           copyWords[i][j].status = "correct";
         }
       }
     }
-
-    // backspace inside word checker
-    if (words[phraseLength - 1][wordLength] != undefined) {
-      if (words[phraseLength - 1][wordLength].status != "") {
-        copyWords[phraseLength - 1][wordLength].status = "";
-      }
-    }
-
-    // backspace between words checker
-    if (words[phraseLength][0].status != "") {
-      copyWords[phraseLength] = copyWords[phraseLength].map(x => (x.status = ""))
-    }
-
-    // set current active word
-    // if (copyWords[phraseLength - 1][wordLength] == undefined) {
-    //   copyWords[phraseLength][0].status = "active"
-    // } else {
-    //   copyWords[phraseLength - 1][wordLength].status = "active";
-    // }
-
     setWords([...copyWords]);
   };
+  
+  // const handleTime = () => {
+  //   setStarted(true);
+  //   if (started == false) {
+  //     setTimeout(() => {
+  //       setTime((value) => value - amount)
+  //     }, 1000);
+  //   }
+  // }
 
   return (
     <div className="typetestContainer">
       <div className="typetestWrapper">
         <div className="typetestInfo">
-          <span id="wpm">73</span>
+          <span id="wpm">74</span>
           <span>&nbsp;WPM</span>
           <span>&nbsp;/&nbsp;</span>
-          <span id="time">63</span>
+          <span id="time">{time}</span>
           <span>&nbsp;s</span>
         </div>
         <div className="typetestInput">
@@ -94,9 +77,10 @@ export default function Typetest() {
             autoFocus={true}
             onBlur={({ target }) => target.focus()}
             onChange={handleInput}
+            // onKeyDown={handleTime}
           />
         </div>
-        <div className="typetestText" id="wordsList">
+        <div className="typetestText">
           {words.map((word, i) => (
             <div className="words" key={i}>
               {word.map((letter, j) => (
