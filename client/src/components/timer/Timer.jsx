37eeda8d93b987d4f663bc05fc17cbe7ce1bfb0f
Lucas Roberto <lucas.roberto.01@outlook.com>
2023-05-18 22:33:54 -0300

    feature_timer added pause

--- client/src/components/timer/Timer.jsx
+++ client/src/components/timer/Timer.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect, useRef } from "react";
 const DEFAULT_SECONDS = 3
 
 export default function Timer() {
-  const [started, setStarted] = useState(false);
+  const [status, setStatus] = useState('Ended');
   const [seconds, setSeconds] = useState(DEFAULT_SECONDS);
   const inputSeconds = useRef(DEFAULT_SECONDS)
   
@@ -13,31 +13,53 @@ export default function Timer() {
         return --prevSeconds
       } else {
         endTest()
+        return inputSeconds
       }
     })
   };
+
+  const increaseSeconds = () => {
+    setSeconds((prevSeconds) => {
+      return ++prevSeconds
+    })
+  };
+
+  //uncomment to check if interval is running even after ending
+  // useEffect(() => {
+  //   console.log('test')
+  // }, [seconds])
   
   useEffect(() => {
-    let timer
-    if (started) {
-      timer = setInterval(decreaseSeconds, 1000)
+    console.log(`has ${status}`);
+    let intervalID
+    if (status == 'Started') {
+      intervalID = setInterval(inputSeconds.current == 0 ? increaseSeconds : decreaseSeconds, 1000)
     }
-    return () => clearInterval(timer)
-  }, [started])
+    return () => clearInterval(intervalID)
+  }, [status])
 
+  //bug? if you click start multiple times around the time it reaches 0, the program crashes
   const startTest = () => {
-    if (!started) {
-      setStarted(true)
+    if (status == 'Paused') {
+      setSeconds(seconds)
+      //setStatus('Started')
+    } else if(status == 'Ended') {
       setSeconds(inputSeconds.current)
-      console.log('has Started')
+      //setStatus('Started')
+    }
+    setStatus('Started')
+  }
+
+  const pauseTest = () => {
+    if (status == 'Started') {
+      setStatus('Paused')
     }
   }
   
   const endTest = () => {
-    if (started) {
+    if (status == 'Started') {
       setSeconds(0)
-      setStarted(false)
-      console.log('has Ended')
+      setStatus('Ended')
     }
   }
   
@@ -53,6 +75,8 @@ export default function Timer() {
       <input type='text' onChange={startTest}></input>
       <input type='text' onKeyDown={handleKeyDown}></input>
       <button onClick={startTest}>Start</button>
+      <button onClick={startTest}>Resume</button>
+      <button onClick={pauseTest}>Pause</button>
       <button onClick={endTest}>End</button>
       <button></button>
     </div>
