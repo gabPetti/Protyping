5068fa774a46b3c12f37b25984126268dd82c79a
gabPetti <gabpettidev@gmail.com>
2023-05-18 22:39:51 -0300

    Add dashboard

---
+++ client/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import "./dashboard.sass"
+
+export default function Dashboard() {
+  return (
+    <div className="dashboardContainer">
+      <div className="dashboardWrapper"></div>
+    </div>
+  )
+}
