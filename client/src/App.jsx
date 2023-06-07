af1e14fbbf687f783d7b5bb56833af57a825f282
gabPetti <gabpettidev@gmail.com>
2023-06-07 00:32:49 -0300

    Change style of App and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import Casual from "./pages/casual/Casual"
 import Login from "./pages/login/Login"
 import Register from "./pages/register/Register"
+import Navbar from "./components/navbar/Navbar"
 import "./App.css"
 
-function App() {
+function AnimatedRoutes() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <BrowserRouter>
+    <TransitionGroup>
+      <CSSTransition key={location.key} timeout={500} classNames="fade">
         <Routes>
-          <Route exact path="/" element={<Casual />} />
+          <Route exact path="/casual" element={<Casual />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           {/* <Route path="/leaderboards" element={<Leaderboards />} /> */}
         </Routes>
-      </BrowserRouter>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <AnimatedRoutes />
+      </Router>
     </div>
   )
 }
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,19 +2,30 @@ import "./navbar.sass";
 import { useNavigate } from "react-router-dom";
 import TypeIt from "typeit-react";
 
-
 export default function Navbar() {
+  const navigate = useNavigate()
+
+  function navigateToPage(location) {
+    navigate(location);
+  }
+
   return (
     <header className="navbarContainer">
       <div className="navbarTop">
+        <div className="config">
+          <button className="configButton"></button>
+        </div>
         <TypeIt element={"h1"} className="logo">Protyping</TypeIt>
+        <div className="profile">
+          <button onClick={() => navigateToPage("/login")} className="profileButton">Sign In</button>
+        </div>
       </div>
       <nav className="navbarBottom">
         <ul>
-          <li><a href="">Casual</a></li>
-          <li><a href="">Ranked</a></li>
-          <li><a href="">Leaderboards</a></li>
-          <li><a href="">Profile</a></li>
+          <li><a href="" onClick={() => navigateToPage("/casual")}>Casual</a></li>
+          <li><a href="" onClick={() => navigateToPage("/ranked")}>Ranked</a></li>
+          <li><a href="" onClick={() => navigateToPage("/leaderboards")}>Leaderboards</a></li>
+          <li><a href="" onClick={() => navigateToPage("/profile")}>Profile</a></li>
         </ul>
       </nav>
     </header>
