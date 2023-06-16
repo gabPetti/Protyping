bee08a60cc175dc80c87ee838fc775cd83021362
gabPetti <gabpettidev@gmail.com>
2023-06-16 00:26:27 -0300

    Add login and register authentification

--- server/index.js
+++ server/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
-dotenv.config();
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('connected to MongoDB')).catch(err => console.log(err));
 
 // routes
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
 const wordsRoute = require("./routes/words");
 
 //middleware
@@ -15,7 +23,8 @@ app.use(express.json());
 app.use(morgan("common"));
 app.use(helmet());
 
-// app.use("/users", userRoute);
+app.use("/server/auth", authRoute);
+app.use("/server/users", usersRoute);
 app.use("/server/words", wordsRoute);
 
 app.listen(8800, () => {
